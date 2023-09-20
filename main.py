78388372373a7b993dff2c34bbbcc18d62f0af23
amartyanambiar <amartyanambiar@gmail.com>
2023-09-20 10:46:27 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Create a sample DataFrame
+data = {'Column1': [1, 2, 3, 4], 'Column2': ['A', 'B', 'C', 'D']}
+df = pd.DataFrame(data)
+
+# Save the DataFrame to a CSV file
+df.to_csv('1.csv', index=False)
