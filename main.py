6662b8faa7eec9a0dcbd5ae2725f096e9868c28a
amartyanambiar <amartyanambiar@gmail.com>
2023-09-20 10:48:59 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
+save_path = os.path.join(os.getcwd(), '1.csv')
+
 # Create a sample DataFrame
 data = {'Column1': [1, 2, 3, 4], 'Column2': ['A', 'B', 'C', 'D']}
 df = pd.DataFrame(data)
 
 # Save the DataFrame to a CSV file
-df.to_csv('1.csv', index=False)
+df.to_csv(save_path, index=False)
